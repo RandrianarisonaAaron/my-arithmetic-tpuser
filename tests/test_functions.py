42ac85eedfce9402c6eed929bee33ec2322ca6c2
RandrianarisonaAaron <aaron.randrianarisona@etudiant.univ-lr.fr>
2023-11-22 16:27:02 +0100

    added more test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,5 +5,10 @@ from unittest import TestCase
 class TestFunctions(TestCase):
     
     def test_pgcd(self):
-        self.assertEqual(31, gcd(40902, 24140))
+        self.assertEqual(34, gcd(40902, 24140))
 
+    def test_pgcd2(self):
+        self.assertEqual(1, gcd(1, 97))    
+
+    def test_pgcd3(self):
+        self.assertEqual(12, gcd(36, 120))
